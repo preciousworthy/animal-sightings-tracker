4d385f559424b46f6527b84e7a651145b00dbdd8
Precious Worthy <preciousworthy@gmail.com>
2022-09-27 11:24:34 -0700

    initial setup of app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Students from "./components/students";
+import Header from "./components/header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Students />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/header.js
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <div>
+            <h1>The Animal Sightings Tracker for Endangered Species</h1>
+            <h3>Welcome, friends! This app is dedicated to tracking some of our endangered friends.</h3>
+        </div>
+    )
+}
+export default Header;
